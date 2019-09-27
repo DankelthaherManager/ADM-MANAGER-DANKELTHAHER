683de15d6ae9cc3408691b52eb38b5e028ad339e
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2019-09-27 20:36:40 -0300

    Update Script

--- request/nload.sh
+++ request/nload.sh
@@ -2,7 +2,7 @@
 Block="/etc/bin" && [[ ! -d ${Block} ]] && exit
 Block > /dev/null 2>&1
 clear
-cowsay -f eyes "con esta herramienta puedes el trafico de red...." | lolcat 
+cowsay -f eyes "Con esta herramienta puedes ver el trafico de red...." | lolcat 
 figlet ..dankelthaher.. | lolcat
 echo -e "\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
 echo -e "\033[1;31mPARA SALIR DEL PANEL PRESIONE CTLR+C \e[0m"
