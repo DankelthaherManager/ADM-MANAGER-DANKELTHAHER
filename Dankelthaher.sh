af592015e9395b42aea1dc9817fd622d82e86fc7
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2020-07-29 11:11:47 -0400

    Update Script

--- Dankelthaher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/DankelthaherManager/ADM-MANAGER-DANKELTHAHER/master/instala.sh; chmod 777 instala.sh* && ./instala.sh*
-
